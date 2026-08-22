76d294540df4c2f4f772255e1138609a53f44a24
Task153 Migration <task153-migration@local>
2026-08-22 23:18:11 +0800

    chore: initialize sanitized green bug environment

---
+++ internal/fragment/window.go
@@ -0,0 +1,17 @@
+package fragment
+
+import (
+	"task153-rfinterference/internal/model"
+	"time"
+)
+
+const LateWindow = 20 * time.Minute
+
+func IsLate(now time.Time, fragment model.Fragment) bool {
+	return now.Sub(fragment.CorrectedAt) > LateWindow
+}
+func OverlapsFrequency(a, b model.Fragment, toleranceHz int64) bool {
+	amin, amax := model.FrequencyRange(a.CenterHz, a.BandwidthHz)
+	bmin, bmax := model.FrequencyRange(b.CenterHz, b.BandwidthHz)
+	return !(amin <= bmax+toleranceHz && bmin <= amax+toleranceHz)
+}
